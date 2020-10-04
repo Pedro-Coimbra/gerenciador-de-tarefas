345b1f5110d0c0eaa2df48afd62d5aa48633799f
pedro.santos1 <pedro.santos@sempreceub.com>
2020-10-04 18:04:16 -0300

    adicao do componente de cadastrar tarefa; o cadastrar tarefa ja esta funcionando normalmente

--- src/app/tarefas/tarefas-routing.module.ts
+++ src/app/tarefas/tarefas-routing.module.ts
@@ -1,14 +1,20 @@
 import { Routes } from "@angular/router";
 
 import { ListarTarefaComponent } from './listar';
+import { CadastrarTarefaComponent } from "./cadastrar";
 
 export const TarefaRoutes: Routes = [
   {
     path: 'tarefas',
-    redirectTo: 'tarefa/listar'
+    redirectTo: 'tarefas/listar'
   },
   {
     path: 'tarefas/listar',
     component: ListarTarefaComponent
   },
-]
+  {
+    path: 'tarefas/cadastrar',
+    component: CadastrarTarefaComponent
+  },
+
+];
