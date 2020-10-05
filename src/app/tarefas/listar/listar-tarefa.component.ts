4722d8e773a99ae0a51add84547b7b96c2f07b91
pedro.santos1 <pedro.santos@sempreceub.com>
2020-10-05 09:45:06 -0300

    adicao do excluir e concluir tarefa

--- src/app/tarefas/listar/listar-tarefa.component.ts
+++ src/app/tarefas/listar/listar-tarefa.component.ts
@@ -21,4 +21,18 @@ export class ListarTarefaComponent implements OnInit {
     return this.TarefaService.listarTodos();
   }
 
+  remover($event: any, tarefa: Tarefa): void {
+    $event.preventDefault();
+    if (confirm('Deseja remover a tarefa "' + tarefa.nome + '"?')) {
+      this.TarefaService.remover(tarefa.id);
+      this.tarefas = this.TarefaService.listarTodos();
+    }
+  }
+
+  alterarStatus(tarefa: Tarefa): void {
+    if (confirm('Deseja alterar o status da tarefa "' + tarefa.nome + '"?')) {
+      this.TarefaService.alterarStatus(tarefa.id);
+      this.tarefas = this.listarTodos();
+    }
+  }
 }
